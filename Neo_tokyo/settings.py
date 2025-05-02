0f346a13189af0c55ec46ed355577acdbaff6321
pramod <gopinath.pramod@gmail.com>
2025-05-02 17:20:22 +0530

    email edit

--- Neo_tokyo/settings.py
+++ Neo_tokyo/settings.py
@@ -224,8 +224,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ecartshoppingtest@gmail.com'
-EMAIL_HOST_PASSWORD = 'annsizhjwccpztrj'
+EMAIL_HOST_USER = "cepcentre007@gmail.com"
+EMAIL_HOST_PASSWORD = 'sjcapvmtthbkdhuh'
 
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = "69145476126-31qfbt7ehrgopm6uka09end6hchl2e4j.apps.googleusercontent.com"       # Google Client ID
