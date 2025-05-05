41a5034ec5055cf9d7bb2fe1bab157855043ab82
pramod <gopinath.pramod@gmail.com>
2025-05-05 14:36:04 +0530

    image field update

--- interactions/models.py
+++ interactions/models.py
@@ -89,7 +89,7 @@ class GrievanceTicket(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True, related_name='grivience_on_product')
     product_name = models.CharField(max_length=255, null=True, blank=True)
 
-    image = models.FileField(upload_to="grievance_image", null=True, blank=True)
+    image = models.ImageField(upload_to="grievance_image", null=True, blank=True)
     link  = models.URLField(null=True, blank=True)
 
     product_serial_number = models.CharField(max_length=100, null=True, blank=True)
